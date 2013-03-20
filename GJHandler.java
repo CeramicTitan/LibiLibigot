8b25253fbb4530a46d06476361985efbbbc1d9f6
T00thpick1 <t00thpick1dirko@gmail.com>
2013-03-20 02:55:56 -0400

    GJ is nubz

---
+++ GJHandler.java
@@ -0,0 +1,18 @@
+package awesome.drtshock;
+
+import awesome.t00thpick1.libilibigot.LibiLibigot;
+import awesome.t00thpick1.Handler;
+
+public class GJHandler implements Handler {
+
+    private static LibiLibigot libi;
+
+    public GJHandler(LibiLibigot libiLibigot) {
+        GJHandler.libi = libiLibigot;
+    }
+    
+    public static void disable() {
+        
+    }
+
+}
