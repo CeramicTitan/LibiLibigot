f1f81e15ee96aba73c4b25ffc4d02f7793084a32
blha303 <stevensmith.ome@gmail.com>
2013-03-20 15:20:07 +0800

    Add blha303handler

--- src/main/java/libilibigot/LibiLibigot.java
+++ src/main/java/libilibigot/LibiLibigot.java
@@ -1,5 +1,7 @@
 package libilibigot;
 
+import java.util.List;
+
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -7,6 +9,7 @@ import com.drtshock.awesome.DrtShockHandler;
 import awesome.gj.GJHandler;
 import awesome.t00thpick1.libilibigot.T00thpick1Handler;
 import libilibigot.utilzbiznath.Handler;
+import me.blha303.Blha303Handler;
 
 
 public class LibiLibigot extends JavaPlugin {
@@ -15,6 +18,8 @@ public class LibiLibigot extends JavaPlugin {
     public static LibiLibigot libiLibigot;
     public static DrtShockHandler drtshockHandler;
     public static GJHandler gjHandler;
+    public static Blha303Handler blha303handler
+    public static T00thpick1Handler t00thHandler;
     public List<Handler> handlers = new ArrayList<Handler>();
     public void onEnable() {
         LibiLibigot.libiLibigot = this;
@@ -22,9 +27,11 @@ public class LibiLibigot extends JavaPlugin {
         LibiLibigot.drtshockHandler = new DrtShockHandler(this);
         LibiLibigot.gjHandler = new GJHandler(this);
         LibiLibigot.t00thHandler = new T00thpick1Handler(this);
+        LibiLibigot.blha303handler = new Blha303Handler(this);
         handlers.add(LibiLibigot.t00thHandler);
         handlers.add(LibiLibigot.drtshockHandler);
         handlers.add(LibiLibigot.gjHandler);
+        handlers.add(LibiLibigot.blha303handler);
     }
     public void onDisable() {
         for(Handler handle: handlers) {
---
+++ src/main/java/me/blha303/Blha303Handler.java
@@ -0,0 +1,19 @@
+package me.blha303;
+
+import libilibigot.LibiLibigot;
+import libilibigot.utilzbiznatch.Handler;
+
+public class Blha303Handler implements Handler {
+	
+	private static LibiLibigot a;
+
+	public Blha303Handler(LibiLibigot libiLibigot) {
+		Blha303Handler.a = libiLibigot;
+	}
+
+	public void disable() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
