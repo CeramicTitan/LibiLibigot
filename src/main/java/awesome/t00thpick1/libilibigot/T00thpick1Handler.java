61191fe3fd54ba4b94941d7511d5d91e26e594b2
blha303 <stevensmith.ome@gmail.com>
2013-03-20 15:00:35 +0800

    Update main class

---
+++ src/main/java/awesome/t00thpick1/libilibigot/T00thpick1Handler.java
@@ -0,0 +1,18 @@
+package awesome.t00thpick1.libilibigot;
+
+import libilibigot.LibiLibigot;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.drtshock.awesome.DrtShockHandler;
+
+public class T00thpick1Handler {
+    
+    private static LibiLibigot a;
+
+    public T00thpick1Handler(LibiLibigot libiLibigot) {
+        T00thpick1Handler.a = libiLibigot;
+        a();
+    }
+}
