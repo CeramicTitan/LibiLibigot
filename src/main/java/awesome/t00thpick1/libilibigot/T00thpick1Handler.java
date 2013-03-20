3497f1802813782b2fe4edcc9901fcae81805c79
T00thpick1 <t00thpick1dirko@gmail.com>
2013-03-20 03:08:52 -0400

    Handler

--- src/main/java/awesome/t00thpick1/libilibigot/T00thpick1Handler.java
+++ src/main/java/awesome/t00thpick1/libilibigot/T00thpick1Handler.java
@@ -1,18 +1,23 @@
 package awesome.t00thpick1.libilibigot;
 
 import libilibigot.LibiLibigot;
+import libilibigot.utilzbiznatch.Handler;
 
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.drtshock.awesome.DrtShockHandler;
 
-public class T00thpick1Handler {
+public class T00thpick1Handler implements Handler {
     
     private static LibiLibigot a;
 
     public T00thpick1Handler(LibiLibigot libiLibigot) {
         T00thpick1Handler.a = libiLibigot;
         a();
+    }
+    
+    public void disable() {
+        
     }
 }
--- src/main/java/libilibigot/LibiLibigot.java
+++ src/main/java/libilibigot/LibiLibigot.java
@@ -5,6 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.drtshock.awesome.DrtShockHandler;
 import awesome.gj.GJHandler;
+import awesome.t00thpick1.libilibigot.T00thpick1Handler;
+import libilibigot.utilzbiznath.Handler;
 
 
 public class LibiLibigot extends JavaPlugin {
@@ -13,10 +15,20 @@ public class LibiLibigot extends JavaPlugin {
     public static LibiLibigot libiLibigot;
     public static DrtShockHandler drtshockHandler;
     public static GJHandler gjHandler;
+    public List<Handler> handlers = new ArrayList<Handler>();
     public void onEnable() {
         LibiLibigot.libiLibigot = this;
         LibiLibigot.plugin = libiLibigot;
         LibiLibigot.drtshockHandler = new DrtShockHandler(this);
         LibiLibigot.gjHandler = new GJHandler(this);
+        LibiLibigot.t00thHandler = new T00thpick1Handler(this);
+        handlers.add(LibiLibigot.t00thHandler);
+        handlers.add(LibiLibigot.drtshockHandler);
+        handlers.add(LibiLibigot.gjHandler);
+    }
+    public void onDisable() {
+        for(Handler handle: handlers) {
+            handle.disable();
+        }
     }
 }
