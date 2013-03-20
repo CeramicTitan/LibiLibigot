e2911c4ad67aaa977fdc3f375bf57022bea73bd7
T00thpick1 <t00thpick1dirko@gmail.com>
2013-03-20 02:58:21 -0400

    Gj

--- src/main/java/libilibigot/LibiLibigot.java
+++ src/main/java/libilibigot/LibiLibigot.java
@@ -4,6 +4,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.drtshock.awesome.DrtShockHandler;
+import awesome.gj.GJHandler;
 
 
 public class LibiLibigot extends JavaPlugin {
@@ -11,9 +12,11 @@ public class LibiLibigot extends JavaPlugin {
     public static Plugin plugin;
     public static LibiLibigot libiLibigot;
     public static DrtShockHandler drtshockHandler;
+    public static GJHandler gjHandler;
     public void onEnable() {
         LibiLibigot.libiLibigot = this;
         LibiLibigot.plugin = libiLibigot;
         LibiLibigot.drtshockHandler = new DrtShockHandler(this);
+        LibiLibigot.gjHandler = new GJHandler(this);
     }
 }
