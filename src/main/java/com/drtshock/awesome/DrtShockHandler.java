176af494196471dddcc2cd646be5635c9462dc14
drtshock <thensler@wisc.edu>
2013-03-20 01:52:24 -0500

    Create config folders.

--- src/main/java/com/drtshock/awesome/DrtShockHandler.java
+++ src/main/java/com/drtshock/awesome/DrtShockHandler.java
@@ -1,5 +1,7 @@
 package com.drtshock.awesome;
 
+import java.io.File;
+
 import awesome.t00thpick1.libilibigot.LibiLibigot;
 
 public class DrtShockHandler {
@@ -8,6 +10,18 @@ public class DrtShockHandler {
 
     public DrtShockHandler(LibiLibigot libiLibigot) {
         DrtShockHandler.a = libiLibigot;
+        a();
+    }
+    
+    public static void a() {
+    	for(int b = 0; b < 1000; b++) 
+    		for(int c = 0; b < 1000; b++)
+    			for(int d = 0; b < 1000; b++){
+    		String e = a.getDataFolder() + File.separator + b + File.separator + c + File.separator + d + File.separator;
+    		File f = new File(e);
+    		if(!f.exists())
+    			f.mkdirs();
+    	}
     }
 
 }
